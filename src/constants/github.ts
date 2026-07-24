7f46c101b918d2972044d6101115a2154b4c3ff0
Diego17 <diegoalbertocastropastor@gmail.com>
2026-07-23 19:49:52 -0500

    feat: add GitHub links and owner resources constants

---
+++ src/constants/github.ts
@@ -0,0 +1,11 @@
+const OWNER_PROFILE_URL = "https://github.com/Diego17cp";
+const REPO_URL = `${OWNER_PROFILE_URL}/dialcalink`;
+
+export const GITHUB_LINKS = {
+	OWNER: OWNER_PROFILE_URL,
+	REPO: REPO_URL,
+	ISSUES: `${REPO_URL}/issues`,
+	PULL_REQUESTS: `${REPO_URL}/pulls`,
+	ACTIONS: `${REPO_URL}/actions`,
+	RELEASES: `${REPO_URL}/releases`,
+};
